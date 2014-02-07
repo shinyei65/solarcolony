23401dae138fef04d7867239019751e342143d1f
Chetna <chetna81.aggarwal@gmail.com>
2014-02-07 11:45:07 -0800

    Initial Commit

---
+++ SolarColony/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  SolarColony
+//
+//  Created by Student on 2/7/14.
+//  Copyright solarcolonyteam 2014. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SolarColony/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  SolarColony
+//
+//  Created by Student on 2/7/14.
+//  Copyright solarcolonyteam 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
