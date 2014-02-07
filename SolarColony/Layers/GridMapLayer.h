8fb693fbd016241d3171c6f8339102684f9e5203
Student <gpadmin@wireless-plus-059-079.usc.edu>
2014-02-07 15:49:08 -0800

    Change the Models Folder setting

---
+++ SolarColony/Layers/GridMapLayer.h
@@ -0,0 +1,16 @@
+//
+//  GridMapLayer.h
+//  SolarColony
+//
+//  Created by Student on 2/7/14.
+//  Copyright 2014 solarcolonyteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GridMapLayer : CCLayer {
+    
+}
+
+@end
---
+++ SolarColony/Layers/GridMapLayer.m
@@ -0,0 +1,14 @@
+//
+//  GridMapLayer.m
+//  SolarColony
+//
+//  Created by Student on 2/7/14.
+//  Copyright 2014 solarcolonyteam. All rights reserved.
+//
+
+#import "GridMapLayer.h"
+
+
+@implementation GridMapLayer
+
+@end
