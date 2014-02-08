72f728f800bd302bb3a2d92c4cdbdf49a39a5446
Po-Yi Lee <po-yilee@Po-Yis-MacBook-Air.local>
2014-02-08 00:39:55 -0800

    Create Friends Layer.

---
+++ SolarColony/Layers/Friends.h
@@ -0,0 +1,13 @@
+//
+//  Friends.h
+//  SolarColony
+//
+//  Created by Po-Yi Lee on 2/8/14.
+//  Copyright (c) 2014 solarcolonyteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Friends : NSObject
+
+@end
---
+++ SolarColony/Layers/Friends.m
@@ -0,0 +1,13 @@
+//
+//  Friends.m
+//  SolarColony
+//
+//  Created by Po-Yi Lee on 2/8/14.
+//  Copyright (c) 2014 solarcolonyteam. All rights reserved.
+//
+
+#import "Friends.h"
+
+@implementation Friends
+
+@end
