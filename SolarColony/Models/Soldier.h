b2d49d47f43bd75abdb95fa43bbc6e8cc0094153
Student <gpadmin@wireless-plus-039-015.usc.edu>
2014-02-09 14:51:32 -0800

    create soldier model

---
+++ SolarColony/Models/Soldier.h
@@ -0,0 +1,34 @@
+//
+//  Soldier.h
+//  SolarColony
+//
+//  Created by Student on 2/9/14.
+//  Copyright 2014 solarcolonyteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Soldier : CCNode {
+    
+}
+
++ (instancetype) attacker:(int)health ATTACK:(int)attack Speed:(int)speed ATTACK_SP:(int)attack_sp;
++ (instancetype) runner:(int)health ATTACK:(int)attack Speed:(int)speed ATTACK_SP:(int)attack_sp;
+- (instancetype) attacker_init;
+- (instancetype) runner_init;
+- (void)setHEALTH:(int)health;
+- (int)getHEALTH;
+- (void)setATTACK:(int)attack;
+- (int)getATTACK;
+- (void)setATTACK_SP:(int)attack_sp;
+- (int)getATTACK_SP;
+- (void)setSPEED:(int)speed;
+- (int)getSPEED;
+- (void)setPOSITION:(int)x Y:(int)y; //set the position of soldier in grid coordinate
+- (CGPoint)getPOSITION;
+
+
+
+
+@end
---
+++ SolarColony/Models/Soldier.m
@@ -0,0 +1,14 @@
+//
+//  Soldier.m
+//  SolarColony
+//
+//  Created by Student on 2/9/14.
+//  Copyright 2014 solarcolonyteam. All rights reserved.
+//
+
+#import "Soldier.h"
+
+
+@implementation Soldier
+
+@end
