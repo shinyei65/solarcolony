142dd5c2cf1beed53ff38c361f9dc67510f3f852
Po-Yi Lee <po-yilee@guest-wireless-upc-nat-206-117-88-085.usc.edu>
2014-02-10 10:57:12 -0800

    Create Settings.h and Settings.m

---
+++ SolarColony/Layers/Settings.h
@@ -0,0 +1,13 @@
+//
+//  Settings.h
+//  SolarColony
+//
+//  Created by Po-Yi Lee on 2/10/14.
+//  Copyright (c) 2014 solarcolonyteam. All rights reserved.
+//
+
+#import "AbstractScene.h"
+
+@interface Settings : CCLayer<AbstractScene>
+
+@end
