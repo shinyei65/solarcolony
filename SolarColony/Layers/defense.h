05fca473eb12aff1af8d890b28c5c9bae092fdac
Chetna <chetna81.aggarwal@gmail.com>
2014-02-10 11:48:28 -0800

    create defense page

---
+++ SolarColony/Layers/defense.h
@@ -0,0 +1,16 @@
+//
+//  defense.h
+//  SolarColony
+//
+//  Created by Student on 2/10/14.
+//  Copyright 2014 solarcolonyteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface defense : defense {
+    
+}
+
+@end
---
+++ SolarColony/Layers/defense.m
@@ -0,0 +1,14 @@
+//
+//  defense.m
+//  SolarColony
+//
+//  Created by Student on 2/10/14.
+//  Copyright 2014 solarcolonyteam. All rights reserved.
+//
+
+#import "defense.h"
+
+
+@implementation defense
+
+@end
