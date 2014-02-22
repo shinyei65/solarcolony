4883b2e467e709a183afd32b3fbe5341a2f30816
Student <gpadmin@wireless-plus-053-007.usc.edu>
2014-02-21 16:19:38 -0800

    added menu selection tower

--- SolarColony/Layers/DefenseSettingsScene.m
+++ SolarColony/Layers/DefenseSettingsScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "DefenseSettingsScene.h"
+#import "CCScrollLayer.h"
 
 @implementation DefenseSettingsScene
 @synthesize mobileDisplaySize;
@@ -40,8 +41,9 @@
         
         [self addChild:splash];
         [self addChild:[self loadMenu]];
-        [self addChild:[self loadMutableTowersMenu]];
-        [self addChild:[self loadSelectorTowersMenu]];
+        //[self addChild:[self loadMutableTowersMenu]];
+      //  [self addChild:[self loadSelectorTowersMenu]];
+        [self addChild:[self scrollLayer]];
     }
     return self;
 }
@@ -116,6 +118,92 @@
     } else if ([menuItem.label.string isEqualToString:@"TowerG"]) {
         [item7 setString:@"TowerG"];
     }
+}
+
+// Creates new Scroll Layer with pages returned from scrollLayerPages.
+- (CCScrollLayer *) scrollLayer
+{
+    CGSize screenSize = [CCDirector sharedDirector].winSize;
+    
+    // Create the scroller and pass-in the pages (set widthOffset to 0 for fullscreen pages).
+    CCScrollLayer *scroller = [CCScrollLayer nodeWithLayers: [self scrollLayerPages] widthOffset: 0.48f * screenSize.width ];
+    scroller.pagesIndicatorPosition = ccp(screenSize.width * 0.5f, screenSize.height - 30.0f);
+    
+    // New feature: margin offset - to slowdown scrollLayer when scrolling out of it contents.
+    // Comment this line or change marginOffset to screenSize.width to disable this effect.
+    scroller.marginOffset = 0.5f * screenSize.width;
+    
+    return scroller;
+}
+
+// Returns array of CCLayers - pages for ScrollLayer.
+- (NSArray *) scrollLayerPages
+{
+    CGSize screenSize = [CCDirector sharedDirector].winSize;
+    
+    // PAGE 1 - Simple Label in the center.
+    CCLayer *pageOne = [CCLayer node];
+    
+    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Page 1" fontName:@"Arial Rounded MT Bold" fontSize:44];
+    label.position = ccp( screenSize.width *.7 , screenSize.height/2 );
+    [pageOne addChild:label];
+    
+    CCLayer *pageTwo = [CCLayer node];
+   
+    CCLabelTTF *label2 = [CCLabelTTF labelWithString:@"Page 2" fontName:@"Arial Rounded MT Bold" fontSize:44];
+    label2.position = ccp( screenSize.width *.7 , screenSize.height/2 );
+    [pageTwo addChild:label2];
+    
+    CCLayer *page4= [CCLayer node];
+    [page4 addChild:[self getMenuForTower:@"Tower 3" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page5= [CCLayer node];
+    [page5 addChild:[self getMenuForTower:@"Tower 4" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page6=[CCLayer node];
+    [page6 addChild:[self getMenuForTower:@"Tower 5" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page7=[CCLayer node];
+    [page7 addChild:[self getMenuForTower:@"Tower 6" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page8=[CCLayer node];
+    [page8 addChild:[self getMenuForTower:@"Tower 7" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page9=[CCLayer node];
+    [page9 addChild:[self getMenuForTower:@"Tower 8" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page10=[CCLayer node];
+    [page10 addChild:[self getMenuForTower:@"Tower 9" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    CCLayer *page11=[CCLayer node];
+    [page11 addChild:[self getMenuForTower:@"Tower 10" Location:ccp( screenSize.width *.7 , screenSize.height/2 )]];
+    
+    // PAGE 2 - Custom Font Menu in the center.
+   /*CCLayer *pageTwo = [CCLayer node];
+    CCLabelTTF *labelTwo = [CCLabelTTF labelWithString:@"Add Page!" fontName:@"Marker Felt" fontSize:44];
+    CCMenuItemLabel *titem = [CCMenuItemLabel itemWithLabel:labelTwo target:self selector:@selector(addPagePressed:)];
+    CCLabelTTF *labelTwo2 = [CCLabelTTF labelWithString:@"Remove Page!" fontName:@"Marker Felt" fontSize:44];
+    CCMenuItemLabel *titem2 = [CCMenuItemLabel itemWithLabel:labelTwo2 target:self selector:@selector(removePagePressed:)];
+    CCLabelTTF *labelTwo3 = [CCLabelTTF labelWithString:@"Change dots color!" fontName:@"Marker Felt" fontSize:40];
+    CCMenuItemLabel *titem3 = [CCMenuItemLabel itemWithLabel:labelTwo3 target:self selector:@selector(changeColorPressed:)];
+    CCMenu *menu = [CCMenu menuWithItems: titem, titem2, titem3, nil];
+    [menu alignItemsVertically];
+    menu.position = ccp(screenSize.width*.7, screenSize.height/2);
+    [pageTwo addChild:menu];*/
+    
+    
+    return [NSArray arrayWithObjects: pageOne,pageTwo,page4,page5,page6,page7,page8,page9,page10,page11,nil];
+}
+
+- (CCMenu*) getMenuForTower:(NSString*) titlePage  Location: (CGPoint) location{
+   
+        
+    CCLabelTTF *labelTwo = [CCLabelTTF labelWithString:titlePage fontName:@"Marker Felt" fontSize:44];
+    CCMenuItemLabel *titem = [CCMenuItemLabel itemWithLabel:labelTwo target:self selector:@selector(addPagePressed:)];
+    CCMenu *menu = [CCMenu menuWithItems: titem,  nil];
+    [menu alignItemsVertically];
+    menu.position = location;
+    return menu;
 }
 
 -(void)moveToScene:(id)sender{
