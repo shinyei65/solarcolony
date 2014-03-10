5c227660db473fc1ac130804c856739ad85a4e7f
Student <gpadmin@wireless-plus-055-082.usc.edu>
2014-03-10 11:10:20 -0700

    repush soldiers

---
+++ SolarColony/Managers/NetWorkManager.h
@@ -0,0 +1,13 @@
+//
+//  NetWorkManager.h
+//  SolarColony
+//
+//  Created by Student on 3/10/14.
+//  Copyright (c) 2014 solarcolonyteam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetWorkManager : NSObject
+
+@end
