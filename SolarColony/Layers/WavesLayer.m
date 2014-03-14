16eab67627235e04bcd58024df2d25a04a726664
ElvisLee0725 <elvislee0725@gmail.com>
2014-03-14 01:34:07 -0700

    Modify WavesLayer.m and when each wave is selected, the color will turn red!

--- SolarColony/Layers/WavesLayer.m
+++ SolarColony/Layers/WavesLayer.m
@@ -58,22 +58,22 @@
         //Game status global variables
         gameStatusEssentialsSingleton=[GameStatusEssentialsSingleton sharedInstance];
         
-        CCMenuItemFont *wave1=[CCMenuItemFont itemWithString:@"Wave 1" target:self selector:@selector(setSoldierinWave:)];
-        item1=[CCMenuItemFont itemWithString:@"Wave 2" target:self selector:@selector(setSoldierinWave:)];
-        item2=[CCMenuItemFont itemWithString:@"Wave 3" target:self selector:@selector(setSoldierinWave:)];
-        item3=[CCMenuItemFont itemWithString:@"Wave 4" target:self selector:@selector(setSoldierinWave:)];
-        item4=[CCMenuItemFont itemWithString:@"Wave 5" target:self selector:@selector(setSoldierinWave:)];
-        item5=[CCMenuItemFont itemWithString:@"Wave 6" target:self selector:@selector(setSoldierinWave:)];
+        item1=[CCMenuItemFont itemWithString:@"Wave 1" target:self selector:@selector(setSoldierinWave:)];
+        item2=[CCMenuItemFont itemWithString:@"Wave 2" target:self selector:@selector(setSoldierinWave:)];
+        item3=[CCMenuItemFont itemWithString:@"Wave 3" target:self selector:@selector(setSoldierinWave:)];
+        item4=[CCMenuItemFont itemWithString:@"Wave 4" target:self selector:@selector(setSoldierinWave:)];
+        item5=[CCMenuItemFont itemWithString:@"Wave 5" target:self selector:@selector(setSoldierinWave:)];
+        item6=[CCMenuItemFont itemWithString:@"Wave 6" target:self selector:@selector(setSoldierinWave:)];
        
         
-        [wave1 setFontSize:20];
         [item1 setFontSize:20];
         [item2 setFontSize:20];
         [item3 setFontSize:20];
         [item4 setFontSize:20];
         [item5 setFontSize:20];
+        [item6 setFontSize:20];
         
-        CCMenu *waveMenus= [CCMenu menuWithItems: wave1,item1, item2, item3, item4, item5, nil];
+        CCMenu *waveMenus= [CCMenu menuWithItems:item1, item2, item3, item4, item5, item6, nil];
         //[WaveMenus  alignItemsInGridWithPadding:ccp(25, 25) columns:3];
         
         [waveMenus alignItemsVertically];
@@ -92,18 +92,62 @@
     
     if ([menuItem.label.string isEqualToString:@"Wave 1"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w1"];
+         [menuItem setColor:ccc3(255,0,0)];
+        
+        [item2 setColor:ccc3(255,255,255)];
+        [item3 setColor:ccc3(255,255,255)];
+        [item4 setColor:ccc3(255,255,255)];
+        [item5 setColor:ccc3(255,255,255)];
+        [item6 setColor:ccc3(255,255,255)];
+        
     }else if ([menuItem.label.string isEqualToString:@"Wave 2"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w2"];
+         [menuItem setColor:ccc3(255,0,0)];
+        
+        [item1 setColor:ccc3(255,255,255)];
+        [item3 setColor:ccc3(255,255,255)];
+        [item4 setColor:ccc3(255,255,255)];
+        [item5 setColor:ccc3(255,255,255)];
+        [item6 setColor:ccc3(255,255,255)];
     }else if ([menuItem.label.string isEqualToString:@"Wave 3"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w3"];
+         [menuItem setColor:ccc3(255,0,0)];
+        
+        [item1 setColor:ccc3(255,255,255)];
+        [item2 setColor:ccc3(255,255,255)];
+        [item4 setColor:ccc3(255,255,255)];
+        [item5 setColor:ccc3(255,255,255)];
+        [item6 setColor:ccc3(255,255,255)];
     }else if ([menuItem.label.string isEqualToString:@"Wave 4"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w4"];
+         [menuItem setColor:ccc3(255,0,0)];
+        
+        [item1 setColor:ccc3(255,255,255)];
+        [item2 setColor:ccc3(255,255,255)];
+        [item3 setColor:ccc3(255,255,255)];
+        [item5 setColor:ccc3(255,255,255)];
+        [item6 setColor:ccc3(255,255,255)];
     }else if ([menuItem.label.string isEqualToString:@"Wave 5"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w5"];
+         [menuItem setColor:ccc3(255,0,0)];
+        
+        [item1 setColor:ccc3(255,255,255)];
+        [item2 setColor:ccc3(255,255,255)];
+        [item3 setColor:ccc3(255,255,255)];
+        [item4 setColor:ccc3(255,255,255)];
+        [item6 setColor:ccc3(255,255,255)];
     }else if ([menuItem.label.string isEqualToString:@"Wave 6"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w6"];
+         [menuItem setColor:ccc3(255,0,0)];
+        
+        [item1 setColor:ccc3(255,255,255)];
+        [item2 setColor:ccc3(255,255,255)];
+        [item3 setColor:ccc3(255,255,255)];
+        [item4 setColor:ccc3(255,255,255)];
+        [item5 setColor:ccc3(255,255,255)];
     }else if ([menuItem.label.string isEqualToString:@"Wave 7"]) {
         [gameStatusEssentialsSingleton setCurrentWave:@"w7"];
+         [menuItem setColor:ccc3(255,0,0)];
     }
 }
 -(void)dealloc{
